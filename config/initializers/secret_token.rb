e10cfd4016cd08f806dcaadfd820d4ba82cb4a84
Fernando Briano <fernando@picandocodigo.net>
2011-11-18 01:20:03 -0200

    Generated Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RuBeer::Application.config.secret_token = '906b46260a3185561cf6a6601b21ff6ebc3a91d7ddc407d41a7622c1e3b998f3ebf8fc9a176be1d3408979cfd506b6ec3ac999c4211538d5c88a8046bd3c4a34'
