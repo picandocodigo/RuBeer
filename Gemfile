32ac5db309eb90b39146cddf6c68b8433bbe300f
Juan Manuel García <jmg.utn@gmail.com>
2011-12-07 01:02:30 -0300

    other stuff to get along with heroku

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ gem 'pg'
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
 
 group :development do
   gem 'rspec-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ RuBeer::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   
-  root :to => 'place#index'
+  root :to => 'maps#index'
 
   # See how all your routes lay out with "rake routes"
 
