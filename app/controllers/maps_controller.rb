688e9ffb3a47a3fc6031f0ac5b6000c07c18361a
Juan Manuel García <jmg.utn@gmail.com>
2011-12-08 15:52:44 -0300

    bootstrap stylized

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,11 +3,31 @@ class MapsController < ApplicationController
     def index
         
         @places = Place.all
+        @place = Place.new
         
         respond_to do |format|
             format.html 
             format.json { render json: @places }
         end
+        
+    end
+        
+    def create
+    
+        @place = Place.new(params[:place])
+
+        respond_to do |format|
+        
+            if @place.save                
+                format.html { render :text => "sucess" }
+                format.xml  { render :xml => @place, :status => :created, :location => @place }                
+            else                
+                format.html { render :text => "fail" }
+                format.xml  { render :xml => @place.errors, :status => :unprocessable_entity }
+            end
+            
+        end
+        
     end
 
 end
